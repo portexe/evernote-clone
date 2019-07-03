e622502f6536e8e7c5ed78213c6c3f6e7b3b1e93
Zack Wilson <zackazt@gmail.com>
2019-07-03 17:49:28 -0400

    Compete tutorial

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB_Pq5AHrbKM2te-Ky1Zm45BqIodKLLNX0",
+  authDomain: "evernote-clone-44896.firebaseapp.com",
+  databaseURL: "https://evernote-clone-44896.firebaseio.com",
+  projectId: "evernote-clone-44896",
+  storageBucket: "evernote-clone-44896.appspot.com",
+  messagingSenderId: "915706065297",
+  appId: "1:915706065297:web:103bb9bcb967aef5"
+});
+
+ReactDOM.render(<App />, document.getElementById('evernote-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
