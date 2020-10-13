2a7d2a425a1a5fc779671c95bca54fbe54f1a8ee
NickTSA <45651986+NickTSA@users.noreply.github.com>
2020-10-12 20:37:23 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-const firebase = require('firebase');
-require('firebase/firestore');
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyB_Pq5AHrbKM2te-Ky1Zm45BqIodKLLNX0",
-  authDomain: "evernote-clone-44896.firebaseapp.com",
-  databaseURL: "https://evernote-clone-44896.firebaseio.com",
-  projectId: "evernote-clone-44896",
-  storageBucket: "evernote-clone-44896.appspot.com",
-  messagingSenderId: "915706065297",
-  appId: "1:915706065297:web:103bb9bcb967aef5"
+  apiKey: "AIzaSyDrEaVM8VCSoK53fTX6pgHHwMSXuxfvs98",
+  authDomain: "my-first-firebase-projec-2a252.firebaseapp.com",
+  databaseURL: "https://my-first-firebase-projec-2a252.firebaseio.com",
+  projectId: "my-first-firebase-projec-2a252",
+  storageBucket: "my-first-firebase-projec-2a252.appspot.com",
+  messagingSenderId: "432137360523",
+  appId: "1:432137360523:web:7311c544d6d8c434e0708e",
+  measurementId: "G-3MVN7NL3HD"
 });
 
-ReactDOM.render(<App />, document.getElementById('evernote-container'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
